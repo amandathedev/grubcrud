ebcf79282e055ccd6ad664d0fdfd792cfecdae80
Amanda Treutler <amandathedev@gmail.com>
2019-10-11 09:43:43 -0500

    postgres

--- db/migrate/20190729193208_create_items.rb
+++ db/migrate/20190729193208_create_items.rb
@@ -1,11 +1,12 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
+      t.integer :restaurant_id
       t.string :name
       t.integer :price
       t.string :img_url
       t.string :description
-      t.references :restaurant, foreign_key: true
+      # t.references :restaurant, foreign_key: true
 
       t.timestamps
     end
