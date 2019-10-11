6279605a285ab43afde81e1a0cfac5a0811cb17d
Amanda Treutler <amandathedev@gmail.com>
2019-10-11 11:43:27 -0500

    references

--- db/migrate/20190729193030_create_orders.rb
+++ db/migrate/20190729193030_create_orders.rb
@@ -1,7 +1,8 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
-      t.references :user, foreign_key: true
+      t.integer :user_id
+      # t.references :user, foreign_key: true
       t.boolean :status
       t.integer :total_price
 
